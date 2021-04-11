8bb82df24494086d96dd5991ca199ea6af0249d6
pyaepyaelily <pyaepyaelily@gmail.com>
2021-04-11 23:06:17 +0800

    Logic for minesweeper works, need to work on the smiley button

--- mini_assignment/src/minesweeper/testFrame.java
+++ mini_assignment/src/minesweeper/testFrame.java
@@ -2,9 +2,23 @@ package minesweeper;
 
 import java.awt.*;        // Use AWT's Layout Manager
 import java.awt.event.*;  // Use AWT's Event handlers
+import java.io.File;
+import java.io.FileInputStream;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Random;  // Use to generate a random int for mines position
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;     // Use Swing's Containers and Components
-
+//import sun.audio.AudioPlayer;
+//import sun.audio.AudioStream;
 
 /**
  * The Mine Sweeper Game. Left-click to reveal a cell. Right-click to
@@ -13,24 +27,24 @@ import javax.swing.*;     // Use Swing's Containers and Components
  * mine.
  */
 @SuppressWarnings("serial")
-public final class testFrame implements MouseListener, ActionListener {
-
+public final class testFrame{
     //From another minesweeper project
     private JFrame screen = null;
-    private JPanel midPanel = new JPanel();
+    private JPanel contentPanel = new JPanel();
+    private JPanel buttonPanel = new JPanel();
     private JPanel topPanel = new JPanel();
-
     private JButton smiley = new JButton("");
+    ImageIcon smileyImageIcon = null;
 
 
     // Name-constants for the game properties
-    public int rows = 5; //0 to get user's input
-    public int columns = 5; //0 to get user's input
-
-//    Container cp = this.getContentPane();
-
+    public int rows = 0;
+    public int columns = 0;
+    public Container cp;
     // Name-constants for UI control (sizes, colors and fonts)
     public static final int CELL_SIZE = 60;  // Cell width and height, in pixels
+    //    public static final int CANVAS_WIDTH = CELL_SIZE * COLS; // Game board width/height
+//    public static final int CANVAS_HEIGHT = CELL_SIZE * ROWS;
     public static final Color BGCOLOR_NOT_REVEALED = Color.GREEN;
     public static final Color FGCOLOR_NOT_REVEALED = Color.RED;    // flag
     public static final Color BGCOLOR_REVEALED = Color.DARK_GRAY;
@@ -54,127 +68,112 @@ public final class testFrame implements MouseListener, ActionListener {
     // Constructor to set up all the UI and game components
     public testFrame() {
         // Reusable input variable
-//        String input;
-//        boolean exitFlag = false;
-//        // make a dynamic game modifier
-//        do {
-//            input = JOptionPane.showInputDialog(
-//                    null,
-//                    "How many rows would you want your mine sweeper have? (enter an integer value > 1)",
-//                    "Message",
-//                    JOptionPane.QUESTION_MESSAGE
-//            );
-//            // Some basic regex to detect digits and only degits since Java doesn't have a type safe parsing
-//            if (input == null) {
-//                System.exit(0);
-//                return;
-//            }
-//            if (input.trim().matches("\\d+")) {
-//                rows = Integer.parseInt(input.trim());
-//                if (rows <= 1) {
-//                    exitFlag = true;
-//                    continue;
-//                }
-//                exitFlag = false;
-//            } else {
-//                exitFlag = true;
-//            }
-//        } while (exitFlag);
-//
-//        // reset the value of the flag for reuse
-//        exitFlag = false;
-//        do {
-//            input = JOptionPane.showInputDialog(
-//                    null,
-//                    "How many columns would you want your mine sweeper have? (enter an integer value > 1)",
-//                    "Message",
-//                    JOptionPane.QUESTION_MESSAGE
-//            );
-//            if (input == null) {
-//                System.exit(0);
-//                return;
-//            }
-//            if (input.trim().matches("\\d+")) {
-//                columns = Integer.parseInt(input.trim());
-//                if (columns <= 1) {
-//                    exitFlag = true;
-//                    continue;
-//                }
-//                exitFlag = false;
-//            } else {
-//                exitFlag = true;
-//            }
-//        } while (exitFlag);
-//
-//        exitFlag = false;
-//        do {
-//            input = JOptionPane.showInputDialog(
-//                    null,
-//                    "How many mines do you want to have? (enter an integer value > 0 and <= " + ((columns * rows) - 1) + ")",
-//                    "Message",
-//                    JOptionPane.QUESTION_MESSAGE);
-//            if (input == null) {
-//                System.exit(0);
-//                return;
-//            }
-//            if (input.trim().matches("\\d+")) {
-//                numMines = Integer.parseInt(input.trim());
-//                if (numMines < 0 || numMines > ((columns * rows) - 1)) {
-//                    exitFlag = true;
-//                    continue;
-//                }
-//                exitFlag = false;
-//            } else {
-//                exitFlag = true;
-//            }
-//        } while (exitFlag);
-//
-        btnCells = new JButton[5][5];
-        mines = new boolean[5][5];
-//        flags = new boolean[rows][columns];
+        String input;
+        boolean exitFlag = false;
+        // make a dynamic game modifier
+        do {
+            input = JOptionPane.showInputDialog(
+                    null,
+                    "How many rows would you want your mine sweeper have? (enter an integer value > 1)",
+                    "Message",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+            // Some basic regex to detect digits and only degits since Java doesn't have a type safe parsing
+            if (input == null) {
+                System.exit(0);
+                return;
+            }
+            if (input.trim().matches("\\d+")) {
+                rows = Integer.parseInt(input.trim());
+                if (rows <= 1) {
+                    exitFlag = true;
+                    continue;
+                }
+                exitFlag = false;
+            } else {
+                exitFlag = true;
+            }
+        } while (exitFlag);
 
+        // reset the value of the flag for reuse
+        exitFlag = false;
+        do {
+            input = JOptionPane.showInputDialog(
+                    null,
+                    "How many columns would you want your mine sweeper have? (enter an integer value > 1)",
+                    "Message",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+            if (input == null) {
+                System.exit(0);
+                return;
+            }
+            if (input.trim().matches("\\d+")) {
+                columns = Integer.parseInt(input.trim());
+                if (columns <= 1) {
+                    exitFlag = true;
+                    continue;
+                }
+                exitFlag = false;
+            } else {
+                exitFlag = true;
+            }
+        } while (exitFlag);
+
+        exitFlag = false;
+        do {
+            input = JOptionPane.showInputDialog(
+                    null,
+                    "How many mines do you want to have? (enter an integer value > 0 and <= " + ((columns * rows) - 1) + ")",
+                    "Message",
+                    JOptionPane.QUESTION_MESSAGE);
+            if (input == null) {
+                System.exit(0);
+                return;
+            }
+            if (input.trim().matches("\\d+")) {
+                numMines = Integer.parseInt(input.trim());
+                if (numMines < 0 || numMines > ((columns * rows) - 1)) {
+                    exitFlag = true;
+                    continue;
+                }
+                exitFlag = false;
+            } else {
+                exitFlag = true;
+            }
+        } while (exitFlag);
+
+        btnCells = new JButton[rows][columns];
+        mines = new boolean[rows][columns];
+        flags = new boolean[rows][columns];
 
         screen = new JFrame();
-        //topPanel.setLayout(new BorderLayout());
-        GridLayout gLayout = new GridLayout(5, 5, 2, 2);
-        midPanel.setLayout(gLayout);
 
         //button to put in topPanel
-        JButton button = new JButton("smiley icon here");
-        topPanel.add(button, BorderLayout.PAGE_START);
-
-        //Content Pane
-//        Container cp = midPanel.getContentPane();           // JFrame's content-pane
-//        cp.setLayout(new GridLayout(5, 5, 2, 2)); // in 10x10 GridLayout
-
+        smiley.setPreferredSize(new Dimension(25, 25));
+        smiley.setIcon(smileyImageIcon);
+//        smileyImageIcon = getScaledImage("mini_assignment/images/smiley.png");
+        topPanel.add(smiley);
 
         JPanel contentPane = new JPanel();
         contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
         contentPane.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 5, 10, 10));
+        buttonPanel.setLayout(new GridLayout(rows, columns, 10, 10));
 
-        CellMouseListener listener = new CellMouseListener();
-        numRevealed = 0;
-
-
-        for (int row = 0; row < 5; row++) {
-            for (int col = 0; col < 5; col++) {
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
                 btnCells[row][col] = new JButton();  // Allocate each JButton of the array
-                btnCells[row][col].setEnabled(true);  // enable button
-                btnCells[row][col].setForeground(FGCOLOR_NOT_REVEALED);
-                btnCells[row][col].setBackground(BGCOLOR_NOT_REVEALED);
-                btnCells[row][col].setFont(FONT_NUMBERS);
-                btnCells[row][col].addMouseListener(listener);
-                btnCells[row][col].setText("");       // display blank
-                mines[row][col] = false;   // clear all the mines
-//                flags[row][col] = false;   // clear all the flags
                 buttonPanel.add(btnCells[row][col]);          // add to content-pane in GridLayout
 
+                // Add MouseEvent listener to process the left/right mouse-click
+                // ... [TODO 4]
+                // btnCells[row][col].addMouseListener(listener);
             }
         }
         contentPane.add(buttonPanel);
+
         screen.add(topPanel, BorderLayout.NORTH);
         screen.add(contentPane, BorderLayout.SOUTH);
 
@@ -187,7 +186,7 @@ public final class testFrame implements MouseListener, ActionListener {
         initGame();
     }
 
-    //     Initialize and re-initialize a new game
+    // Initialize and re-initialize a new game
     private void initGame() {
         // Reset cells, mines, and flags
         CellMouseListener listener = new CellMouseListener();
@@ -220,151 +219,60 @@ public final class testFrame implements MouseListener, ActionListener {
         }
     }
 
+    /**
+     * *
+     * Overload method to re-initialize the game with new difficulty
+     *
+     * @param newRow
+     * @param newCol
+     * @param newNumMines
+     */
+//    private void initGame(int newRow, int newCol, int newNumMines) {
+//        rows = newRow;
+//        columns = newCol;
+//        numMines = newNumMines;
+//
+//        btnCells = new JButton[rows][columns];
+//        mines = new boolean[rows][columns];
+//        flags = new boolean[rows][columns];
+//
+//        cp = this.getContentPane();
+//        cp.removeAll();
+//        cp.setLayout(new GridLayout(rows, columns, 2, 2));
+//
+//        for (int row = 0; row < rows; row++) {
+//            for (int col = 0; col < columns; col++) {
+//                btnCells[row][col] = new JButton();
+//                cp.add(btnCells[row][col]);
+//            }
+//        }
+//
+//        cp.setPreferredSize(new Dimension(CELL_SIZE * columns, CELL_SIZE * rows));
+//        pack();
+//
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setTitle("Mineswepper");
+//        setVisible(true);
+//        initGame();
+//    }
+
 
     // The entry main() method
     public static void main(String[] args) {
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File("./Last-Surprise.wav"));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            //woops
+        }
+
         //[TODO 1]
         testFrame ms = new testFrame();
     }
 
-    //ask the user if they want to replay the game with the same difficulty or the same
-//    public void askContinue() {
-//        int confirmation = JOptionPane.showConfirmDialog(
-//                null,
-//                "Do you want to play again",
-//                "Continue",
-//                JOptionPane.YES_NO_OPTION,
-//                JOptionPane.QUESTION_MESSAGE
-//        );
-//
-//        if (confirmation == JOptionPane.YES_OPTION) {
-//            confirmation = JOptionPane.showConfirmDialog(
-//                    null,
-//                    "Do you want to change the difficulty?",
-//                    "Too difficult?",
-//                    JOptionPane.YES_NO_OPTION,
-//                    JOptionPane.QUESTION_MESSAGE
-//            );
-//            if (confirmation == JOptionPane.NO_OPTION) {
-//                initGame();
-//                return;
-//            } else if (confirmation == JOptionPane.CANCEL_OPTION) {
-//                System.exit(0);
-//            } else if (confirmation == JOptionPane.YES_OPTION) {
-//                String input;
-//                int newRow = -1;
-//                int newCol = -1;
-//                int newNumMines = -1;
-//                boolean exitFlag = false;
-//                // make a dynamic game modifier
-//                do {
-//                    input = JOptionPane.showInputDialog(
-//                            null,
-//                            "How many rows would you want your mine sweeper have? (enter an integer value > 1)",
-//                            "Message",
-//                            JOptionPane.QUESTION_MESSAGE
-//                    );
-//                    // Some basic regex to detect digits and only deguts since Java doesn't have a type safe parsing
-//                    if (input == null) {
-//                        System.exit(0);
-//                        return;
-//                    }
-//                    if (input.trim().matches("\\d+")) {
-//                        newRow = Integer.parseInt(input.trim());
-//                        if (newRow < 1) {
-//                            exitFlag = true;
-//                            continue;
-//                        }
-//                        exitFlag = false;
-//                    } else {
-//                        exitFlag = true;
-//                    }
-//                } while (exitFlag);
-//
-//                // reset the value of the flag for reuse
-//                exitFlag = false;
-//                do {
-//                    input = JOptionPane.showInputDialog(
-//                            null,
-//                            "How many columns would you want your mine sweeper have? (enter an integer value > 1)",
-//                            "Message",
-//                            JOptionPane.QUESTION_MESSAGE
-//                    );
-//                    if (input == null) {
-//                        System.exit(0);
-//                        return;
-//                    }
-//                    if (input.trim().matches("\\d+")) {
-//                        newCol = Integer.parseInt(input.trim());
-//                        if (newCol < 1) {
-//                            exitFlag = true;
-//                            continue;
-//                        }
-//                        exitFlag = false;
-//                    } else {
-//                        exitFlag = true;
-//                    }
-//                } while (exitFlag);
-//
-//                exitFlag = false;
-//                do {
-//                    input = JOptionPane.showInputDialog(
-//                            null,
-//                            "How many mines do you want to have? (enter an integer value > 0 and <= " + ((newCol * newRow) - 1) + ")",
-//                            "Message",
-//                            JOptionPane.QUESTION_MESSAGE
-//                    );
-//                    if (input == null) {
-//                        System.exit(0);
-//                        return;
-//                    }
-//                    if (input.trim().matches("\\d+")) {
-//                        newNumMines = Integer.parseInt(input.trim());
-//                        if (newNumMines < 0 || newNumMines > ((newCol * newRow) - 1)) {
-//                            exitFlag = true;
-//                            continue;
-//                        }
-//                        exitFlag = false;
-//                    } else {
-//                        exitFlag = true;
-//                    }
-//                } while (exitFlag);
-////                initGame(newRow, newCol, newNumMines);
-//                return;
-//            }
-//        }
-//        System.exit(0);
-//    }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 
     // [TODO 2]
     private class CellMouseListener extends MouseAdapter {
@@ -398,6 +306,13 @@ public final class testFrame implements MouseListener, ActionListener {
                 // [TODO 5] If you hit a mine, game over
                 // Otherwise, reveal the cell and display the number of surrounding mines
                 if (mines[rowSelected][colSelected]) {
+                    try {
+                        InputStream in = new FileInputStream(new File("./boom.wav"));
+//                        AudioStream a = new AudioStream(in);
+//                        AudioPlayer.player.start(a);
+                    } catch (Exception ex) {
+                        //woops
+                    }
                     for (int row = 0; row < rows; ++row) {
                         for (int col = 0; col < columns; ++col) {
                             if (mines[row][col]) {
@@ -415,7 +330,6 @@ public final class testFrame implements MouseListener, ActionListener {
                             "Game Over",
                             JOptionPane.WARNING_MESSAGE
                     );
-//                    askContinue();
                 } else {
                     int surroundingMineNum = calculateMineNumber(rowSelected, colSelected);
                     if (surroundingMineNum == 0) {
@@ -457,7 +371,6 @@ public final class testFrame implements MouseListener, ActionListener {
                         btnCells[row][col].setEnabled(false);
                     }// end of inner for loop
                 }// end of for loop
-//                askContinue();
             }
         }
 
